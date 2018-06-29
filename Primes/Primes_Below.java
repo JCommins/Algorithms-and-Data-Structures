d9ca4d54805b181e445150113b3a60b8dab936b9
JCommins <35921701+JCommins@users.noreply.github.com>
2018-06-29 15:41:41 +0100

    Update Primes_Below.java

--- Primes/Primes_Below.java
+++ Primes/Primes_Below.java
@@ -1,8 +1,8 @@
-/*Number of primes under a limit
-
-The goal is to read in a number N and output the number of primes below that number. 
+/*
+Number of primes under a limit: The goal is to read in a number N and output the number of primes below that number. 
 For example, if N is 11, then output 4, 
-because there are 4 primes below 11, namely, 2, 3, 5, and 7.*/
+because there are 4 primes below 11, namely, 2, 3, 5, and 7.
+*/
 
 import java.util.*; 
 
@@ -20,7 +20,7 @@ public class Primes_Below {
 		
 		for(int i = n-1; i >= 2; i--){ 
 			if(isPrime(i)){
-				//System.out.println(i); 
+
 				count++;
 			}
 		}
