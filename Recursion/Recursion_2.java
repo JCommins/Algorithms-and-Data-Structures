fba41506cea564c7056dad616f7f2327d4758cf3
JCommins <35921701+JCommins@users.noreply.github.com>
2018-07-02 15:45:35 +0100

    Recursion Example 2

--- Recursion/Recursion_2.java
+++ Recursion/Recursion_2.java
@@ -1,9 +1,8 @@
 /*
  * Problem Statement: 
- * 
  * Identify the output that the following Java code produces and explain your reasoning clearly.
- *
  */ 
+
 public class Recursion{
 	public static void main(String[] args){
         System.out.println(method(3));
@@ -11,17 +10,19 @@ public class Recursion{
 
     public static int method(int number){
         if (number > 30){
-        	return 7; 
+           return 7; 
     	}
         System.out.println("hello");
-        //System.out.println(number);
-        //NB. The 8 is ignored until base case is satisfied
-        //then the returned number represents each resolved bracket-case back up the chain until the first method is reached
-        //=> (returned number) - 8 => -1
-        //=> (-1) -8 = -9
-        //=> (-9) -8 = -17
-        //=> (-17) -8 = -25
-        //*N.B. Have to start return back at where the base case was resolved
         return method(number + 7) - 8;  
     }
 }
+
+/*
+   The 8 is ignored until base case is satisfied
+   Then the returned number represents each resolved bracket-case back up the chain until the first method is reached
+   => (returned number) - 8 => -1
+   => (-1) -8 = -9
+   => (-9) -8 = -17
+   => (-17) -8 = -25
+   Note: Have to start return back at where the base case was resolved
+*/
