b573685d0a199953a633b1726bcf605a3528adef
JCommins <35921701+JCommins@users.noreply.github.com>
2018-07-02 15:52:54 +0100

    Amount of Prime numbers below an arbitrary number

--- Primes/Primes_Below.java
+++ Primes/Primes_Below.java
@@ -1,29 +1,23 @@
 /*
-Number of primes under a limit: The goal is to read in a number N and output the number of primes below that number. 
-For example, if N is 11, then output 4, 
-because there are 4 primes below 11, namely, 2, 3, 5, and 7.
-*/
+ * Number of primes under a limit: The goal is to read in a number N and output the number of primes below that number. 
+ * For example, if N is 11, then the output is 4, because there are 4 primes below 11, namely, 2, 3, 5, and 7.
+ */
 
 import java.util.*; 
 
 public class Primes_Below {
-	
-	public static void main(String args[] ) throws Exception{
-		
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Enter a number and find how many prime numbers precede it: ");
-		
-		int n = input.nextInt(); 
-		
+	public static void main(String args[] ) throws Exception{		
+		Scanner input = new Scanner(System.in);		
+		System.out.println("Enter a number and find how many prime numbers precede it: ");		
+		int n = input.nextInt(); 		
 		int count = 0;
 		
 		for(int i = n-1; i >= 2; i--){ 
-			if(isPrime(i)){
-
-				count++;
-			}
+		   if(isPrime(i)){
+		      count++;
+		   }
 		}
+		
 		System.out.print(count);
 		}
 	
@@ -31,9 +25,8 @@ public class Primes_Below {
 	public static boolean isPrime(int x){ 
 		
 		for(int i = 2; i <= Math.sqrt(x); i++){
-		
 			if(x%i == 0){
-				return false; 
+			   return false; 
 			}
 		}	
 		return true; 	
